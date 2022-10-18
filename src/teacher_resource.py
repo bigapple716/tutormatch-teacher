cebf1e962dbbc7bbc74321d794d8f6112ed009d8
bigapple716 <congcong0716@outlook.com>
2022-10-18 16:30:12 -0400

    add database methods for Teacher resource

--- src/teacher_resource.py
+++ src/teacher_resource.py
@@ -1,10 +1,7 @@
 import pymysql
 
-import os
-
 
 class TeacherResource:
-
     def __int__(self):
         pass
 
@@ -19,12 +16,49 @@ class TeacherResource:
         return connection
 
     @staticmethod
-    def get_by_key(key):
-        sql = "SELECT * FROM teacher_schema.teacher_info where id = 1"
+    def _run_sql(sql):
+        # logging.info(sql)
+        print(sql)
         connection = TeacherResource._get_connection()
         cur = connection.cursor()
-        cur.execute(sql, args=key)
-        result = cur.fetchone()
+        cur.execute(sql)
+        result = cur.fetchall()
 
         return result
 
+    @staticmethod
+    def get_teachers_by_id(id):
+        sql = "SELECT * FROM teacher_schema.teacher_info WHERE id = {}".format(id)
+        return TeacherResource._run_sql(sql)
+
+    @staticmethod
+    def get_teachers_by_name(name):
+        sql = "SELECT * FROM teacher_schema.teacher_info WHERE name = '{}'".format(name)
+        return TeacherResource._run_sql(sql)
+
+    @staticmethod
+    def get_teachers_by_price_range(price_min, price_max):
+        sql = "SELECT * FROM teacher_schema.teacher_info WHERE price >= {} AND price <= {}" \
+            .format(price_min, price_max)
+        return TeacherResource._run_sql(sql)
+
+    @staticmethod
+    def get_teachers_by_skills(skills):
+        if len(skills) == 1:
+            sql = "SELECT teacher_id FROM teacher_schema.skills WHERE skill_name = '{}'".format(skills[0])
+        else:
+            sql = '''
+                SELECT teacher_id 
+                FROM (
+                    SELECT teacher_id, count(*) as cnt 
+                    FROM teacher_schema.skills 
+                    WHERE skill_name IN {} 
+                    GROUP BY teacher_id) skill_cnt 
+                WHERE skill_cnt.cnt = {}'''.format(tuple(skills), len(skills))
+        return TeacherResource._run_sql(sql)
+
+
+# main function is only for testing
+if __name__ == '__main__':
+    res = TeacherResource.get_teachers_by_skills(['Python', 'Java'])
+    print(res)
